ddfc198563a1676f9557c982c8178993c7fa9775
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-08-12 00:56:40 -0500

    Completed Exercise 2 w/ math operations methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,26 +4,73 @@
     {
         static void Main(string[] args)
         {
-            Story();
+            //Story();
+
+            int addition = Add(3, 4);
+            Console.WriteLine(addition);
+
+            int subtraction = Subtract(7, 5);
+            Console.WriteLine(subtraction);
+
+            int multiplication = Multiply(6, 2);
+            Console.WriteLine(multiplication);
+
+            int division = Divide(15, 3);
+            Console.WriteLine(division);
+
+            int calculation = Total(2, 4, 6, 8, 10);
+            Console.WriteLine(calculation);
         }
         public static void Story()
         {
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            Console.WriteLine($"Hi {name}, nice to meet you!");
+            Console.WriteLine($"\n Hi {name}, nice to meet you!");
             
             Console.WriteLine("What is your favorite dinner meal?");
             string dinnerMeal = Console.ReadLine();
-            Console.WriteLine($"That sounds really good! {dinnerMeal} is something I'll have to try cooking soon.");
+            Console.WriteLine($"\n That sounds really good! {dinnerMeal} is something I'll have to try cooking soon.");
             
             Console.WriteLine("What is your favorite type of pet?");
             string pet = Console.ReadLine();
-            Console.WriteLine($"Nice, {pet} are fun animals.");
+            Console.WriteLine($"\n Nice, {pet} are fun animals.");
             
             Console.Write("Last questions, who is your favorite musician or band you listen to?");
             string music = Console.ReadLine();
-            Console.WriteLine($"{music} has some great songs! When I was younger, my favorite band was Metallica or AC/DC." +
+            Console.WriteLine($"\n {music} has some great songs! When I was younger, my favorite band was Metallica or AC/DC." +
                               $" Now I mostly listen to country music on the radio or podcasts.");
+        }
+
+        public static int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        public static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public static int Divide(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+
+        public static int Total(params int[] numbers)
+        {
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number; 
+            }
+
+            return sum;
         }
 
     }
